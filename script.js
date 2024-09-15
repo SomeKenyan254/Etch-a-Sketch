d5176cab766b628083591efd5876acf60425ed73
Arnold Korir <144571592+SomeKenyan254@users.noreply.github.com>
2024-09-14 21:49:07 -0600

    Fixed the for loop to make better divs

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-let size = 16
-function makeGrids() {
+
+function makeGrids(size) {
     let screen = document.querySelector(".container");
-  
+   
+    
     for (let i = 0; i < size; i++) {
-      let column = document.createElement("div");
-      column.classList.add("column");
-      for (let j = 1; j <= size; j++) {
-        let row = document.createElement("div");
-        row.classList.add("row");
-       row.style.border = "2px solid black";
-        row.textContent= (i * size) +j;
-        column.appendChild(row);
-      }
-      screen.appendChild(column);
+        for (let j = 0; j < size; j++) {
+            let column = document.createElement("div");
+       column.classList.add("column");
+       screen.appendChild(column)
+}
+      
     }
+    
   }
   
-  makeGrids();
+  makeGrids(16);
