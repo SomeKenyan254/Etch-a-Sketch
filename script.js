26298f792e44322f7629a2472ff6dd09da407d1a
Arnold Korir <144571592+SomeKenyan254@users.noreply.github.com>
2024-09-24 17:35:08 -0600

    implemented a function to ask user for grid size

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
 //Make a grid sqaure
 const screen = document.querySelector(".container");
 
+const button = document.querySelector("#btn");
 
-//Initial grid Size
-let size =16;
-function makeGrids() {
-    const square = 960 / size
-    
-   for (let i = 0; i < size * size; i++) {
+function makeGrids(size) {
+  
+  const square = 960 / size
+  screen.innerHTML = "";
+    for (let i = 0; i < size * size; i++) {
+          let box = document.createElement("div");
+             box.classList.add("column");
+             screen.style.width = "500px"
+              screen.style.height = "500px"
+            
+             
+             box.style.width = `${square}px`
+            box.style.height = `${square}px`
+            screen.appendChild(box)
       
-            let box = document.createElement("div");
-       box.classList.add("column");
-       
-       box.style.width = `${square}px`
-       box.style.height = `${square}px`
+               box.addEventListener('mouseover',() =>{
+               box.style.background = "red";})
+              } }
+              
+          
+ 
 
- box.addEventListener('mouseover',() =>{
-  box.style.background = "blue";})
+
+ makeGrids(16)
+
+//Ask user for a grid size
+const  newGrid = () => {
+  input = prompt("Enter a grid size")
+  const newSize = Number(input)
+  if(newSize > 1 && newSize <=100){
+
+ makeGrids(newSize)}
+ else{
+  alert ("Enter a value between 1 and 100!!")
+ }
+} ; 
+button.addEventListener("click", newGrid )
+
 
   
 
-     
-     screen.appendChild(box)
-}
+
+ 
 
 
-}   
 
 
-   
 
-  
-  makeGrids();
+
